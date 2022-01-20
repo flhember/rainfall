6c8a7df474a204ea9f88a1d6a8cfd3d0325baa86
Florian HEMBERT <flhember@e2r9p12.clusters.42paris.fr>
2022-01-20 18:21:30 +0100

    save

---
+++ level7/source.c
@@ -0,0 +1,10 @@
+//gcc -fno-stack-protector -z execstack -Wl,-z,norelro
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+void		main(int ac, char **av)
+{
+	char *str = (char *) malloc(20 );
+}
