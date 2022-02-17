73ba218d7cdbad5f88a9b9a359317ed0a4fe3241
Florian HEMBERT <flhember@e2r9p6.clusters.42paris.fr>
2022-02-17 15:54:38 +0100

    Complete bonus2

--- bonus2/source.c
+++ bonus2/source.c
@@ -5,14 +5,41 @@
 #include <string.h>
 #include <unistd.h>
 
-int	main(int ac, char **av)
-{
-	char	save1[40];
-	char	save2[40];
+int language = 0;
 
-	if (ac < 3) 
+void		greetuser(char **av)
+{
+	char 	str[88];
+
+	if (language == 1)
+		strcpy(str, "Hyvää päivää ");
+	if (language == 2)
+		strcpy(str, "Goedemiddag! ");
+	if (language == 0)
+		strcpy(str, "Hello ");
+	puts(strcat(str, av[1]));
+	return ;
+}
+
+int		main(int ac, char **av)
+{
+	char	buf[80];
+	char	*env;
+
+	if (ac != 3) 
 		return (1);
-	strncpy(save1, av[1], 40);
-	strncpy(save2, av[2], 40);
+	bzero(buf, 19);
+	strncpy(buf, av[1], 40);
+	strncpy(&buf+40, av[2], 32);
+	env = getenv("LANG");
+	if (env)
+	{
+		
+		if (!memcmp(env, "fi", 2))
+			language = 1;
+		if (!memcmp(env, "nl", 2))
+			language = 2;
+	}
+	greetuser(av);
 	return (0);
 }
